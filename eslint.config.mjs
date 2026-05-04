25f89451c261b68c302685fc32a184ec42828f73
Louis Nguyen <louis@lt.id.vn>
2026-05-04 18:15:04 +0700

    fix(lint): exclude prisma generated client and seed file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,11 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs','**/prisma.client/**'],
+    ignores: [
+      'eslint.config.mjs',
+      'src/core/prisma/prisma.client/**',
+      'src/core/prisma/seed.ts',
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -29,7 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
-      "prettier/prettier": ["error", { endOfLine: "auto" }],
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
     },
   },
 );
